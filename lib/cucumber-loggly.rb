f561910b2122edfcf5c70e4c97d390f434eea5c2
Brett Weaver <brett@weav.net>
2012-10-13 16:21:50 -0700

    adding connect

--- lib/cucumber-loggly.rb
+++ lib/cucumber-loggly.rb
@@ -1,26 +1,3 @@
-require "loggly-ruby-client"
-require "cucumber-loggly/version"
+require "cucumber-loggly/connect"
 require "cucumber-loggly/steps"
-
-module CucumberLoggly
-  class Connect
-    def initialize(account)
-      @loggly = LogglyRubyClient::Search.new :config_file => config_file,
-                                             :account     => account
-    end
-
-    def search(args)
-      result = @loggly.search :from  => args[:from],
-                              :query => args[:query],
-                              :input => args[:input]
-      result["body"]["numFound"].to_i
-    end
-
-    private
-
-    def config_file
-      "#{ENV['HOME']}/.cucumber-loggly.yml"
-    end
-
-  end
-end
+require "cucumber-loggly/version"
---
+++ lib/cucumber-loggly/connect.rb
@@ -0,0 +1,23 @@
+module CucumberLoggly
+
+  class Connect
+    def initialize(account)
+      @loggly = LogglyRubyClient::Search.new :config_file => config_file,
+                                             :account     => account
+    end
+
+    def search(args)
+      result = @loggly.search :from  => args[:from],
+                              :query => args[:query],
+                              :input => args[:input]
+      result["body"]["numFound"].to_i
+    end
+
+    private
+
+    def config_file
+      "#{ENV['HOME']}/.cucumber-loggly.yml"
+    end
+  end
+
+end
